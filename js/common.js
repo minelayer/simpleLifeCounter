06a29139b239724a3e2d214f77fd6dcd2d22e26f
mine <minelayer.ac@gmail.com>
2015-03-31 19:35:55 +0900

    cookieでの状態保存に対応

--- js/common.js
+++ js/common.js
@@ -14,20 +14,29 @@ $(function () {
 	var initLife = 20;
 	var maxPlayerNumber = 4;
 	var playerNumber = 2;
+	var poisonDisp ='none';
+	var switchScreen ='cancel';
 	var nameDisp = 'hidden';
 	var playerLife = [];
 	var playerPoison = [];
 	var diffLife = [];
+	var nameInput = [];
 	for (var i = 0; i < maxPlayerNumber; i++) {
 		playerLife[i] = initLife;
 		diffLife[i] = 0;
 	};
+	var cookie = $.cookie("setSave");
 
 	var damageLog;
 
 	resizeCounter(playerNumber);
-	initialize();
 	settings();
+
+	if(cookie){
+		cookieLoad();
+	}else{
+		initialize();
+	}
 
 	$(window).resize(function(){
 		resizeCounter(playerNumber);
@@ -48,7 +57,7 @@ $(function () {
 
 	$('.menu .reload').on('tap', function(e){
 		e.preventDefault();
-		$(this).animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0);
+		$(this).stop().animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0);
 		initialize();
 	});
 
@@ -86,6 +95,7 @@ $(function () {
 			$(this).parents('.setting-input').find('ul li').removeClass('active');
 			$(this).parent().addClass('active');
 			setPlayerNumber(playerNumber);
+			cookieSet(null,true);
 		});
 		$('#life-amount span').on('tap', function(e){
 			e.preventDefault();
@@ -93,22 +103,25 @@ $(function () {
 			console.log(initLife);
 			$(this).parent().siblings().removeClass('active');
 			$(this).parent().addClass('active');
+			cookieSet(null,true);
 		});
 		$('#poison-counter span').on('tap', function(e){
 			e.preventDefault();
-			var poisonDisp = $(this).data('poison_counter');
+			poisonDisp = $(this).data('poison_counter');
 			console.log(poisonDisp);
 			$(this).parent().siblings().removeClass('active');
 			$(this).parent().addClass('active');
 			setPoisonDisp(poisonDisp);
+			cookieSet(null,true);
 		});
 		$('#set-fullscreen span').on('tap', function(e){
 			e.preventDefault();
-			var switchScreen = $(this).data('set_fullscreen');
+			switchScreen = $(this).data('set_fullscreen');
 			console.log(switchScreen);
 			$(this).parent().siblings().removeClass('active');
 			$(this).parent().addClass('active');
 			switchScreen == 'set' ? setFullScreen() : cancelFullScreen();
+			cookieSet(null,true);
 		});
 		/*$('#set-neversleep span').on('tap', function(e){
 			e.preventDefault();
@@ -125,6 +138,7 @@ $(function () {
 			$(this).parent().siblings().removeClass('active');
 			$(this).parent().addClass('active');
 			setNameDisp(nameDisp);
+			cookieSet(null,true);
 		});
 		$('#name-input input').on('change', function(){
 			var nameInput = $(this).val();
@@ -133,6 +147,7 @@ $(function () {
 			$(this).parent().siblings().removeClass('active');
 			$(this).parent().addClass('active');
 			setNameInput(nameInput, nameTarget);
+			cookieSet(null,true);
 		});
 	}
 
@@ -179,6 +194,69 @@ $(function () {
 		}
 	}
 
+	function cookieSet(target,settings){
+		$.cookie('setSave','true');
+		if(target){
+			$.cookie("playerPoison["+target+"]",playerPoison[target]);
+			$.cookie("playerLife["+target+"]",playerLife[target]);
+		}else{
+			for (var i = 0; i < maxPlayerNumber; i++) {
+				$.cookie("playerPoison["+i+"]",playerPoison[i]);
+				$.cookie("playerLife["+i+"]",playerLife[i]);
+			};
+		}
+		if(settings){
+			$.cookie("playerNumber",playerNumber);
+			$.cookie("playerNumberActive",$('#player-number ul li').index($('#player-number ul li.active')));
+			$.cookie("initLife",initLife);
+			$.cookie("initLifeActive",$('#life-amount ul li').index($('#life-amount ul li.active')));
+			$.cookie("poisonDisp",poisonDisp);
+			$.cookie("poisonDispActive",$('#poison-counter ul li').index($('#poison-counter ul li.active')));
+			$.cookie("nameDisp",nameDisp);
+			$.cookie("nameDispActive",$('#name-disp ul li').index($('#name-disp ul li.active')));
+			$.cookie("switchScreen",switchScreen);
+			$.cookie("switchScreenActive",$('#set-fullscreen ul li').index($('#set-fullscreen ul li.active')));
+			for (var i = 0; i < maxPlayerNumber; i++) {
+				$.cookie("nameInput["+i+"]",$('#name-input ul li input').eq(i).val());
+			};
+		}
+	}
+
+	function cookieLoad(){ //前回からの続きで表示
+
+		playerNumber = $.cookie("playerNumber");
+		initLife = $.cookie("initLife");
+		poisonDisp = $.cookie("poisonDisp");
+		nameDisp = $.cookie("nameDisp");
+		switchScreen = $.cookie("switchScreen");
+
+		setNameDisp(nameDisp);
+		switchScreen == 'set' ? setFullScreen() : cancelFullScreen();
+		setPlayerNumber(playerNumber);
+		setPoisonDisp(poisonDisp);
+
+		$('#player-number ul li').removeClass('active').eq($.cookie("playerNumberActive")).addClass('active');
+		$('#life-amount ul li').removeClass('active').eq($.cookie("initLifeActive")).addClass('active');
+		$('#poison-counter ul li').removeClass('active').eq($.cookie("poisonDispActive")).addClass('active');
+		$('#name-disp ul li').removeClass('active').eq($.cookie("nameDispActive")).addClass('active');
+		$('#set-fullscreen ul li').removeClass('active').eq($.cookie("switchScreenActive")).addClass('active');
+
+		for (var i = 0; i < maxPlayerNumber; i++) {
+			playerLife[i] = parseInt($.cookie('playerLife['+i+']'));
+			playerPoison[i] = parseInt($.cookie('playerPoison['+i+']'));
+			$(".player").eq(i).find('.life').text(playerLife[i]);
+			$(".player").eq(i).find('.poison').text(playerPoison[i]);
+			$(".player").eq(i).find('.name').text(playerLife[i]);
+
+			nameInput[i] = $.cookie("nameInput["+i+"]");
+
+			setNameInput(nameInput[i], 'player'+(i+1));
+			$('#name-input ul li input').eq(i).val(nameInput[i]);
+			$('#player-number ul li')
+		};
+		resizeCounter();
+	}
+
 	function initialize(){ //初期化
 		for (var i = 0; i < maxPlayerNumber; i++) {
 			playerLife[i] = initLife;
@@ -189,6 +267,7 @@ $(function () {
 		$('.diff').text(0);
 		$('.poison').text(0);
 		resizeCounter();
+		cookieSet(null,true);
 		console.log(damageLog);
 		damageLog = 'start at ' + timeNow() + '\n';
 		console.log('start at ' + timeNow());
@@ -226,6 +305,7 @@ $(function () {
 		var dispDiffLife = diffLife[target] > 0 ? '+' + diffLife[target] : diffLife[target];
 		playerLife[target] = playerLife[target] + amount;
 		var LifeDisp = playerLife[target];
+		cookieSet(target);
 		$(targetID+' .life').text(playerLife[target]);
 		$(targetID+' .diff').text(dispDiffLife).css({'display':'block','opacity':1}).stop().animate({'opacity':0},5000,function(){
 			damageLog += timeNow() + ' ' + targetName + ' ' + dispDiffLife + ' ： 計' + LifeDisp + '\n';
@@ -240,34 +320,36 @@ $(function () {
 		var targetID = '#player'+(parseInt(target)+1);
 		var targetName = $(targetID + ' .name').text();
 		playerPoison[target] = playerPoison[target] == 10 ? 0 : playerPoison[target] + amount;
+		cookieSet(target);
 		$(targetID+' .poison').text(playerPoison[target]);
 		damageLog += timeNow() + ' ' + targetName + ' ' + amount + '毒 ： 計' + playerPoison[target] + '毒\n';
 
 		console.log('life is ' + playerPoison);
 	}
 
-	function onKeyEvent() { //キー押された時の分岐
-		if(window.event.keyCode == 65){ //a
-			lifeEvent(0,-1)
-		}else if(window.event.keyCode == 83){ //s
-			lifeEvent(0,1)
-		}else if(window.event.keyCode == 74){ //j
-			lifeEvent(1,-1)
-		}else if(window.event.keyCode == 75){ //k
-			lifeEvent(1,1)
-		}else if(window.event.keyCode == 82){ //r
-			$('.menu .reload').animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0);
-			initialize();
-		}else if(window.event.keyCode == 81){ //q
-			poisonEvent(0,-1)
-		}else if(window.event.keyCode == 87){ //w
-			poisonEvent(0,1)
-		}else if(window.event.keyCode == 85){ //u
-			poisonEvent(1,-1)
-		}else if(window.event.keyCode == 73){ //i
-			poisonEvent(1,1)
-		}else{return true;}
+	window.document.onkeydown = function(e) { //キー押された時の分岐
+		if($('.settings-block').is(':hidden') && !e.ctrlKey && !e.metaKey){
+			if(e.keyCode == 65){ //a
+				lifeEvent(0,-1)
+			}else if(e.keyCode == 83){ //s
+				lifeEvent(0,1)
+			}else if(e.keyCode == 74){ //j
+				lifeEvent(1,-1)
+			}else if(e.keyCode == 75){ //k
+				lifeEvent(1,1)
+			}else if(e.keyCode == 82){ //r
+				$('.menu .reload').animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0).delay(100).animate({opacity:0},0).delay(100).animate({opacity:1},0);
+				initialize();
+			}else if(e.keyCode == 81){ //q
+				poisonEvent(0,-1)
+			}else if(e.keyCode == 87){ //w
+				poisonEvent(0,1)
+			}else if(e.keyCode == 85){ //u
+				poisonEvent(1,-1)
+			}else if(e.keyCode == 73){ //i
+				poisonEvent(1,1)
+			}else{return true;}
+		}
 	}
-	window.document.onkeydown= onKeyEvent;
 });
 
